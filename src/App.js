b4464f4246544a480da745a1277808827b53aa59
Mark <m.mccormack022@gmail.com>
2020-03-07 23:30:05 +0000

    initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
+            <div className="card card-signin my-5">
+              <div className="card-body">
+                <h5 className="card-title text-center">Security Question</h5>
+                <form className="form-signin">
+                  <div className="form-label-group">
+                  <label htmlFor="inputQuestion">What does Heem mean?</label>
+                    <input type="text" id="inputQuestion" className="form-control" placeholder="Answer..." required autoFocus></input>
+                  </div>
+                  <button className="btn btn-lg btn-primary btn-block text-uppercase" onClick={submit}>Prove your not a casual</button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
+  function submit(e) {
+    e.preventDefault();
+      console.log("clicked");
+  }
 }
 
 export default App;
